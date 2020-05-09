a5e4450e5017eb630ca76e730c9d38d4d5621b9a
Bingqing Zhao <59756429+BingqingZhao@users.noreply.github.com>
2020-05-08 20:56:08 -0700

    Add files via upload

--- e.calculator/script.js
+++ e.calculator/script.js
@@ -1,32 +1,63 @@
 (function(){
     "use strict";
-    const next = document.getElementById("next");
 
-    // next.addEventListener('click',function(evt){
-    //     evt.preventDefault();
-    //     if()
-    // });
+        const Printing = document.getElementById("Printing");
+        const print = document.getElementById("3D_Printing");
+        const Cutting = document.getElementById("Cutting");
+        const cut = document.getElementById("Laser_Cutting");
+
+        Printing.addEventListener("click", function(){
+            print.setAttribute('class', 'visible');
+            cut.setAttribute('class', 'invisible');
+        });
+
+        Cutting.addEventListener("click", function(){
+            print.setAttribute('class', 'invisible');
+            cut.setAttribute('class', 'visible');
+        });
+
+
+
+        // function hola(x) {
+        //     if(x == 0) {
+        //         document.getElementById("3D_Printing").style.visibility="hidden";
+        //         document.getElementById("Laser_Cutting").style.visibility="hidden";
+        //         }
+
+        //     if(x == 1) {
+        //         alert(support_3dprint);
+        //         document.getElementById("3D_Printing").setAttribute('class', 'visible');
+        //         document.getElementById("Laser_Cutting").setAttribute('class', 'invisible');
+        //         // document.getElementById("3D_Printing").style.visibility="visible";
+        //         // document.getElementById("Laser_Cutting").style.visibility="hidden"; 
+        //         }
+
+        //     if(x == 2)  {
+        //         document.getElementById("3D_Printing").style.visibility="hidden";
+        //         document.getElementById("Laser_Cutting").style.visibility="visible"; 
+        //     }
+        // }
+        // hola();
+
+    // function material_calculation(){
+    //     const weight_3dprint = parseFloat(document.getElementById("weight_3dprint_input").value)/1000;
+    //     const support_3dprint = parseFloat(document.getElementById("support_3dprint_input").value)/100 * weight_3dprint;
+    //     support_3dprint = support_3dprint; 
+    //     // .toFixed(4)
+    //     const weight_support_3dprint = weight_3dprint+support_3dprint;
+    //     const total_weight = [support_3dprint, weight_support_3dprint];
+    
+    //     return total_weight;
+    //     // alert(support_3dprint);
+    // };
+    
+    // function PLA_material(){
+    //     let emb_energy_PLA = ((49+54)/2+(0.475+0.525)/2);
+    //     const material_calculation = this.material_calculation();
+    //     let support_3dprint = material_calculation[0];
+    //     const weight_support_3dprint = material_calculation[1];
+    //     alert(weight_support_3dprint.toFixed(4));
+    // }; 
 
 }());
 
-function material_calculation(){
-    var weight_3dprint = parseFloat(document.getElementById("weight_3dprint_input").value)/1000;
-    var support_3dprint = parseFloat(document.getElementById("support_3dprint_input").value)/100 * weight_3dprint;
-    support_3dprint = support_3dprint; 
-    // .toFixed(4)
-    var weight_support_3dprint = weight_3dprint+support_3dprint;
-    var total_weight = [support_3dprint, weight_support_3dprint];
-
-    return total_weight;
-// alert(support_3dprint);
-};
-
-function PLA_material(){
-    var emb_energy_PLA = ((49+54)/2+(0.475+0.525)/2);
-    var material_calculation = this.material_calculation();
-    var support_3dprint = material_calculation[0];
-    var weight_support_3dprint = material_calculation[1];
-    alert(weight_support_3dprint.toFixed(4));
-
-
-}; 
